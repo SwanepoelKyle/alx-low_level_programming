31899eb6e04656e2ccdfd1b0ec9eb0d8827cbf0b
SwanepoelKyle <kyleswanepoel.visual@gmail.com>
2023-03-06 03:39:02 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwasys 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
